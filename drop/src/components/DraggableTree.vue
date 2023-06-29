7158c867838da32a1e1b376c6c185868b2418160
Ram <126403725+Ram85-git@users.noreply.github.com>
2023-06-29 19:26:48 -0400

    Dragging and css addded

--- drop/src/components/DraggableTree.vue
+++ drop/src/components/DraggableTree.vue
@@ -1,87 +1,131 @@
-<!-- DraggableTree.vue -->
+
 <template>
   <h1>Dragging and Dropping</h1>
-  
-  <!-- <div class="drop-zone" @drop="onDrop($event, 1)" @dragenter.prevent @dragover.prevent>
-    <div class="drag-el" draggable="true" @dragstart="startDrag($event, 1)">
-      Parent 1
-    </div>
-    <div v-for="item in getList(1)" :key="item.id" class="drag-el" draggable="true" @dragstart="startDrag($event, item)">
-         <div class="title_area">{{ item.title }}</div> 
-    </div>
-  </div>
-  <div class="drop-zone" @drop="onDrop($event, 2)" @dragenter.prevent @dragover.prevent>
-    <div class="drag-el" draggable="true" @dragstart="startDrag($event, 2)">
-      Parent 2
-    </div>
-    <div v-for="item in getList(2)" :key="item.id" class="drag-el" draggable="true" @dragstart="startDrag($event, item)">
-      
-      <div class="title_area">{{ item.title }}</div> 
-    </div>
-  </div> -->
-  <div class="tree_structure" style="width:446px; height:611px; overflow: auto;">
-    <Tree :value="treeData" :onDrop="onDrop"  edgeScroll style="width:400px; "></Tree>
-  </div>
+  <div class="tree_structure" style="width:546px; height:711px; overflow: auto;">
+    <Tree :value="treeData" :onDrop="onDrop" edgeScroll style="width:400px; ">
+      <template #default="{ node, isSelected, isDropIndicator }">
+        <div :class="{
+          'tree-node': true,
+          'selected-node': isSelected,
+          'drop-indicator': isDropIndicator,
+          'tree-node-back': node.level > 1,
 
+        }" :style="getCustomStyle(node)">
+          {{ node.text }}
+        </div>
+      </template>
+
+
+    </Tree>
+  </div>
 </template>
   
 <script>
 import 'he-tree-vue/dist/he-tree-vue.css'
-import {Tree, Draggable} from 'he-tree-vue'
+import { Tree, Draggable } from 'he-tree-vue'
 export default {
-  components: {Tree: Tree.mixPlugins([Draggable])},
+  components: { Tree: Tree.mixPlugins([Draggable]) },
   data() {
     return {
-      treeData: [{text: 'node 1' , 
-      children: [
-      {text: 'node 1-1'},
-      {text: 'node 1-2'}
-      ]}, 
-      {text: 'node 2', 
-      children: [ 
-      {text: 'node 2-1'},
-      {text: 'node 2-2'}]}, {text: 'node 3',
-      children: [
-      {text: 'node 3-1'},
-      {text: 'node 3-2'}]},
-       {text: 'node 4'}, 
-       {text: 'node 5'}, 
-       {text: 'node 6'}]
+
+      treeData: [
+        {
+
+          text: 'node 1',
+          children: [
+            { text: 'node 1-1' },
+            { text: 'node 1-2' },
+          ]
+        },
+
+        {
+          text: 'node 2',
+          children: [
+            { text: 'node 2-1' },
+            { text: 'node 2-2' }]
+        }, {
+          text: 'node 3',
+          children: [
+            { text: 'node 3-1' },
+            {
+              text: 'node 3-2',
+              children: [
+                { text: 'node 3-1-1' },
+                { text: 'node 3-1-2' }
+              ]
+            }]
+        },
+        { text: 'node 4' },]
     }
   },
   methods: {
+
+    getCustomStyle(node) {
+      const depth = this.getNodeDepth(node);
+      const backgroundColor = depth === 1 ? 'lightblue' : 'lightgreen';
+      const paddingLeft = `${depth * 20}px`; // Adjust the indentation as needed
+
+      let marginLeft = '';
+
+      if (node.parent && node.parent.children) {
+        const index = node.parent.children.indexOf(node);
+
+        if (index % 1 === 0) {
+          marginLeft = '100px';
+        }
+      }
+
+      return {
+        backgroundColor,
+        paddingLeft,
+        marginLeft
+      }
+
+    },
+    getNodeDepth(node) {
+      let depth = 1;
+      let currentNode = node;
+
+      while (currentNode.parent) {
+        depth++;
+        currentNode = currentNode.parent;
+      }
+
+      return depth;
+    },
+
     onDrop(dragData, dropData, type) {
-  if (type === 'inner') {
-    if (!dropData.children) {
-      dropData.children = [dragData];
-    } else if (dropData.children.length < 2) {
-      dropData.children.push(dragData);
+      if (type === 'inner') {
+        if (!dropData.children) {
+          dropData.children = [dragData];
+        } else if (dropData.children.length < 2) {
+          dropData.children.push(dragData);
+        }
+      } else if (type === 'before' || type === 'after') {
+        const parent = dropData.parent;
+        const index = parent.children.indexOf(dropData);
+
+        if (index !== -1) {
+          const targetChildren = parent.children;
+          const targetIndex = targetChildren.indexOf(dropData);
+
+          if (!dropData.children) {
+            dropData.children = [];
+          }
+
+          if (type === 'before') {
+            targetChildren.splice(targetIndex, 0, dragData);
+          } else if (type === 'after') {
+            targetChildren.splice(targetIndex + 1, 0, dragData);
+          }
+
+          if (targetChildren.length > 2) {
+            const newRoot = targetChildren.shift();
+            newRoot.parent = null;
+          }
+        }
+      }
     }
-  } else if (type === 'before' || type === 'after') {
-    const parent = dropData.parent;
-    const index = parent.children.indexOf(dropData);
-
-    if (index !== -1) {
-      const targetChildren = parent.children;
-      const targetIndex = targetChildren.indexOf(dropData);
-      
-      if (!dropData.children) {
-        dropData.children = [];
-      }
-
-      if (type === 'before') {
-        targetChildren.splice(targetIndex, 0, dragData);
-      } else if (type === 'after') {
-        targetChildren.splice(targetIndex + 1, 0, dragData);
-      }
-
-      if (targetChildren.length > 2) {
-        const newRoot = targetChildren.shift();
-        newRoot.parent = null;
-      }
-    }
-  }
-}
   }
 
 }
@@ -111,7 +155,7 @@ export default {
 
 //       if (typeof parentID === 'number') {
 //         event.dataTransfer.setData('parentId', parentID);
-//         event.dataTransfer.setData('childItemId', ''); 
+//         event.dataTransfer.setData('childItemId', '');
 //       } else {
 //         event.dataTransfer.setData('parentId', '');
 //         event.dataTransfer.setData('childItemId', parentID.id.toString());
@@ -154,15 +198,42 @@ export default {
 </script>
   
 <style scoped>
-.tree_structure{
-  border: 1px solid green;
-  padding: 10px;
+.tree_structure {
+  border: 2px solid green;
+  width: 400px;
+  padding-left: 40px;
   margin: auto;
+
   margin-top: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  margin: 20px;
 }
-/* .tree{
-  border: 1px solid green;
-} */
+
+
+.tree-node {
+  background-color: lightgray;
+  border: 1px solid yellow;
+  /* margin: 5px; */
+  border-radius: 5px;
+  /* margin-bottom: 5px; */
+  /* padding: 10px; */
+}
+
+.tree-node.parent {
+  background-color: rgb(67, 223, 5);
+  border: 1px solid;
+  height: 250px;
+}
+
+.selected-node {
+  background-color: rgb(62, 189, 30);
+}
+
+.drop-indicator {
+  background-color: green;
+}
+
 /* .title_area{
   border: 1px solid yellow;
 }
